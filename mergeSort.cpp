3b3de35db3271334ada5288410621f1f0b8ffdb0
Luan Lima <luanlima@MacBook-de-Luan.local>
2018-08-31 10:45:57 -0300

    mergeSort fix

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,55 +1,52 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
-void printArray(int array[], int n) {
-  for (int i=0; i < n; i++) {
-    cout << array[i] << " ";
+void printArray(vector<int> &A, int n) {
+  for (int i=0; i<n; i++) {
+    cout << A[i] << " ";
   }
   cout << endl;
 }
 
-void merge(int array[], int initial, int mid, int end) {
-    int *temp = new int [end-initial];
-    int i = initial;
-    int j = mid;
-    int k = 0;
+void merge(vector<int> &A, int initial, int mid, int end) {
+  int i = initial, j = mid, k = 0, *temp = new int [end-initial];
 
-    while(i<mid && j<end) {
-        if(array[i] <= array[j]) {
-            temp[k]=array[i];
-            i++;
-            k++;
-        } else {
-            temp[k] = array[j];
-            j++;
-            k++;
-        }
+  while (i<mid && j<end) {
+    if (A[i]<=A[j]) {
+      temp[k]=A[i];
+      i++;
+      k++;
+    } else {
+      temp[k] = A[j];
+      j++;
+      k++;
     }
-    while(i<mid) {
-        temp[k] = array[i];
-        i++;
-        k++;
-    }
-    while(j<end) {
-        temp[k] = array[j];
-        j++;
-        k++;
-    }
-
-    for(i = initial; i<end; i++) {
-        array[i] = temp[i-initial];
-    }
+  }
+  while (i<mid) {
+    temp[k] = A[i];
+    i++;
+    k++;
+  }
+  while (j<end) {
+    temp[k] = A[j];
+    j++;
+    k++;
+  }
+  for (i=initial; i<end; i++) {
+  A[i] = temp[i-initial];
+  }
 }
 
-void mergeSort(int array[], int initial, int end) {
-    int mid;
-    if (initial < end-1) {
-        mid = (initial+end)/2;
-        mergeSort(array, initial, mid);
-        mergeSort(array, mid, end);
-        merge(array, initial, mid, end);
+void mergeSort(vector<int> &A, int initial, int end) {
+  int mid;
+    if (initial<end-1) {
+      mid = (initial+end)/2;
+      mergeSort(A, initial, mid);
+      mergeSort(A, mid, end);
+      merge(A, initial, mid, end);
     }
 }
 
@@ -63,13 +60,14 @@ int main() {
   if (input.is_open()) {
     int n;
     input >> n;
-    int array[n];
+    vector<int> A;
+    A.resize(n);
 
     for (int i=0; i<n; i++) {
-      input >> array[i];
+      input >> A[i];
     }
-    mergeSort(array, 0, n);
-    printArray(array, n);
+    mergeSort(A, 0, n);
+    printArray(A, n);
 
   } else cout << "Erro ao abrir o arquivo." << endl;
 
